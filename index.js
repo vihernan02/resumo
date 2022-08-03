b0ff5a5bcda8c41e3a875df0ba4adde314f07c9e
vihernan02 <101815230+vihernan02@users.noreply.github.com>
2022-08-03 11:41:02 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -16,14 +16,25 @@ app.listen(3050,()=>{
 const mongoose = require('mongoose')
 //configurar o script de conexão
 const conexao = ()=>{
-    mongoose.connect('mongodb+srv://userRevisao:revisao1234@fiaptecnico.xtl46.mongodb.net/?retryWrites=true&w=majority')
+    mongoose.connect('mongodb+srv://userRevisao:revisao1234@cluster0.xtl46.mongodb.net/resumo?retryWrites=true&w=majority')
 }
-
+//configurar modelo para coleção alunos
+const modelo = new mongoose.Schema({
+    nome:String,
+    turma:String,
+    disciplina:String
+})
+//definir modelo para coleção alunos
+const alunos = mongoose.model('alunos',modelo)
 /*configuração do banco de dados - fim*/
 
 /*rota para requisição*/
-app.get('/',(req,res)=>{
+app.get('/',async(req,res)=>{
     conexao()
+    //pesquisar os documentos na collection alunos
+    const resultado = await alunos.find()
+    //const resultado = alunos.find()
+    console.log(resultado)
     //res.send('Funcionando')
-    res.render('index.ejs',{nome:"Alunos Feiosos",turma:"2EMIA",disciplina:"LP2"})
-})
+    res.render('index.ejs',{resultado})
+}) 
